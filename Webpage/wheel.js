310593e70d64346320a186e3272a1d5ececc00b3
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2022-06-05 00:18:18 +0100

    Added Pointer

--- Webpage/wheel.js
+++ Webpage/wheel.js
@@ -11,22 +11,34 @@ class fortuneWheel extends HTMLElement{
         this.shadow = this.attachShadow( {mode: "open"} )
         this.shadow.appendChild(document.querySelector("#wheel").content.cloneNode(true));
         this.drawWheel();
+        this.drawPointer();
+    }
+
+    getValues(){
+
     }
 
     update(){
-
+        this.drawWheel();
     }
 
     drawWheel(){
-        this.canvas = this.shadow.querySelector(".wheelCanvas");
-        this.ctx = this.canvas.getContext("2d");
-        this.ctx.fillStyle = "darkblue";
-        this.ctx.arc(200,200,175,0, Math.PI*2);
-        this.ctx.fill();
+        this.wheel = this.shadow.querySelector(".wheelCanvas");
+        this.wheelCtx = this.wheel.getContext("2d");
+        this.wheelCtx.fillStyle = "darkblue";
+        this.wheelCtx.arc(200,200,150,0, Math.PI*2);
+        this.wheelCtx.fill();
     }
 
     drawPointer(){
-
+        this.pointer = this.shadow.querySelector(".pointerCanvas");
+        this.pointCtx = this.pointer.getContext("2d");
+        this.pointCtx.fillStyle = "black";
+        this.pointCtx.beginPath();
+        this.pointCtx.moveTo(380, 185)
+        this.pointCtx.lineTo(380, 215)
+        this.pointCtx.lineTo(350, 200);
+        this.pointCtx.fill();
     }
 
     spinWheel(){
