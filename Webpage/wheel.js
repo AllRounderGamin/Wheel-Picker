c19a65229d14d06e997778f3743c992ff6ef224b
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2022-06-06 05:11:59 +0100

    Wheel Spins Multiple Times

--- Webpage/wheel.js
+++ Webpage/wheel.js
@@ -6,7 +6,7 @@ WHEELTEMP.innerHTML='<link href="./stylesheet.css" rel="stylesheet">\n' +
     '            <canvas class="pointerCanvas" width="400" height="400"></canvas>\n' +
     '        </div>\n' +
     '        <div class="buttonGroup">\n' +
-    '            <button>Spin Wheel</button>\n' +
+    '            <button class="spinButton">Spin Wheel</button>\n' +
     '            <button>Delete Wheel</button>\n' +
     '            <button>Settings</button>\n' +
     '        </div>\n' +
@@ -29,6 +29,7 @@ class fortuneWheel extends HTMLElement{
         this.shadow.appendChild(WHEELTEMP.content.cloneNode(true));
         this.wheel = this.shadow.querySelector(".wheelCanvas");
         this.wheelCtx = this.wheel.getContext("2d");
+        this.shadow.querySelector(".spinButton").addEventListener("click", this.spinWheel.bind(this));
         this.getValues();
         this.drawWheel();
         this.drawPointer();
@@ -48,8 +49,7 @@ class fortuneWheel extends HTMLElement{
     drawWheel(){
         const arcSize = 360 / this.items.length;
         let completion = 0;
-
-        // Draws each part of the circle arc by arc, minuses 90*RADIANCONVERSION so that it starts at the top
+        // Draws each part of the circle arc by arc, minuses 90 * RADIANCONVERSION so that it starts at the top
         for (let i = 0; i < this.items.length; i++){
             this.wheelCtx.beginPath();
             this.wheelCtx.fillStyle = this.colors[i]
@@ -73,11 +73,19 @@ class fortuneWheel extends HTMLElement{
     }
 
     spinWheel(){
-
+        const toSpin = 1080 + (Math.random() * 360);
+        this.wheel.style.setProperty("--s", (this.currentAngle + "deg"));
+        this.wheel.style.setProperty("--e", (Math.floor(this.currentAngle+toSpin) + "deg"));
+        this.wheel.classList.add("active");
+        this.currentAngle = Math.floor(this.currentAngle+toSpin);
+        this.wheel.addEventListener("animationend", this.verifyAnswer.bind(this), {once: true})
     }
 
     verifyAnswer(){
-
+        this.wheel.classList.remove("active");
+        this.wheel.style.setProperty("--r", this.currentAngle + "deg");
+        const winnerPos = ((360 - (this.currentAngle % 360) - 180) % this.items.length);
+        //console.log("Winner is:", this.items[winnerPos]);
     }
 
 }
