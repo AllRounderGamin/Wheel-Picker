3e4aa8ba7151b6cb5177dacdf2c42aafd0fed3d2
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2022-06-11 01:16:27 +0100

    Add Infinite Wheels + Saves

--- Webpage/index.js
+++ Webpage/index.js
@@ -1,8 +1,8 @@
 function init(){
-    const wheelList = localStorage.getItem("wheels)")
+    const wheelList = JSON.parse(localStorage.getItem("wheels"));
     if (wheelList){
-        for (let wheel in wheelList){
-            makeCustomWheel(JSON.parse(wheel))
+        for (let wheel of wheelList){
+            makeCustomWheel(wheel)
         }
     } else{
         makeDefaultWheel()
@@ -10,19 +10,40 @@ function init(){
     if (!localStorage.getItem("next-id")){
         localStorage.setItem("next-id", "1");
     }
+    document.querySelector("#addWheelButton").addEventListener("click", makeNewWheel);
 }
 
 function makeDefaultWheel(){
     const wheel = document.createElement("wheel-picker");
+    const wheelList = [];
     wheel.setAttribute("id", "0");
     wheel.setAttribute("items", '["Placeholder", "Lorem Ipsum", "Test"]');
     wheel.setAttribute("colors", '["red", "blue", "green"]');
     const wheelObj = {
         "id": "0",
-        "items": ["Placeholder", "Lorem Ipsum", "Test"],
-        "colors": ["red", "blue", "green"]
+        "items": '["Placeholder", "Lorem Ipsum", "Test"]',
+        "colors": '["red", "blue", "green"]'
     };
-    localStorage.setItem("wheels", JSON.stringify(wheelObj));
+    wheelList.push(wheelObj)
+    localStorage.setItem("wheels", JSON.stringify(wheelList));
+    document.querySelector("#wheelArea").appendChild(wheel);
+}
+
+function makeNewWheel(){
+    const wheel = document.createElement("wheel-picker");
+    const LS = JSON.parse(localStorage.getItem("wheels"));
+    const id = (parseInt(localStorage.getItem("next-id")) + 1).toString();
+    wheel.setAttribute("id", id)
+    wheel.setAttribute("items", '["Click Settings", "To Customise"]');
+    wheel.setAttribute("colors", '["red", "blue"]');
+    const wheelObj = {
+        "id": id,
+        "items": '["Click Settings", "To Customise"]',
+        "colors": '["red", "blue"]'
+    };
+    LS.push(wheelObj);
+    localStorage.setItem("wheels", JSON.stringify(LS));
+    localStorage.setItem("next-id", id);
     document.querySelector("#wheelArea").appendChild(wheel);
 }
 
